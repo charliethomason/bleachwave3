17b4a5976b3ad42e77bfaa8139f268d58c8d2b52
Charlie Thomason <thomason.charlie@gmail.com>
2013-07-10 23:21:39 -0500

    Removed duplicate code in bleachwave.js and cleaned up formatting.

--- js/bleachwave.js
+++ js/bleachwave.js
@@ -1,29 +1,32 @@
 var $ = jQuery.noConflict();
+
 $(document).ready(function() {
+
 	// Clicking Contact button on homepage displays contact method buttons
 	$("#intro-contact-btn").click(function(e) {
 		e.preventDefault();
 		$("#intro-contact").slideToggle();
 		$(this).toggleClass("clicked");
 	});
+
 	// Clicking anywhere in art cell container takes you to post URL
 	$(".art-cell").click(function() {
 		var artUrl = $(this).find("a.art-cell-img").attr("href");
 		window.open(artUrl, "_self");
 	});
+
 	// Add All Posts link to blog menu
 	$("ul.blog-menu").prepend("<li><a href='/blog'>All Posts</a></li>");
-	// Add button class to 'Continue Reading' links
-	$("a.more-link").addClass("btn grey-btn");
+
 	// Add "current-cat" class to currently-viewed category on category pages
 	if($("div.blog").hasClass("category-blog")) {
 		var categoryTitle = $("#category-title").text();
 		$(".blog-menu a:contains('" + categoryTitle + "')").addClass("current-cat");
 	}
+	
 	// Add grey button class to more links and prev/next post links
 	$("a.more-link, .prev-post a, .next-post a").addClass("btn grey-btn");
-	// Add "All Posts" list item to top of Blog Categories menu
-	$("ul.blog-menu").prepend("<li><a href='/blog'>All Posts</a></li>");
+
 	// Initiate jQuery Masonry for About page
 	$("#facts-box").masonry({
 		itemSelector : '.fact',
@@ -32,4 +35,5 @@ $(document).ready(function() {
 			return containerWidth / 4;
 		}
 	});
+
 });
